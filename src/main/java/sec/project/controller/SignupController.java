1ff3c6110936a638f5ad6f807646cea8d9e45f7e
prinsessv <45902446+prinsessv@users.noreply.github.com>
2018-12-22 09:49:52 +0200

    Update SignupController.java

--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -1,7 +1,10 @@
 package sec.project.controller;
 
+import static java.util.Collections.list;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +17,26 @@ public class SignupController {
     @Autowired
     private SignupRepository signupRepository;
 
-    @RequestMapping("*")
+    @RequestMapping("/")
     public String defaultMapping() {
-        return "redirect:/form";
+       return "redirect:/form";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.GET)
     public String loadForm() {
         return "form";
     }
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String loadAdmin(Model model) {
+        List<Signup> entries = signupRepository.findAll();
+        model.addAttribute("entries", entries);
+        return "admin";
+    }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
+    public String submitForm(Model model, @RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
+        model.addAttribute("name", address);
         return "done";
     }
 
