7a35c3b8cc4b9e2747d275ac589e77d92b59728d
prinsessv <45902446+prinsessv@users.noreply.github.com>
2018-12-22 09:36:57 +0200

    Update CustomUserDetailsService.java

--- src/main/java/sec/project/config/CustomUserDetailsService.java
+++ src/main/java/sec/project/config/CustomUserDetailsService.java
@@ -47,6 +47,6 @@ public class CustomUserDetailsService implements UserDetailsService {
                 true,
                 true,
                 true,
-                Arrays.asList(new SimpleGrantedAuthority("USER")));
+                Arrays.asList(new SimpleGrantedAuthority(role)));
     }
 }
